ae748f7bc49e229530466b728fe21ef769cbb2be
Shubham Tiwari <113251152+ShubhamTiwari04@users.noreply.github.com>
2023-10-05 00:25:27 +0530

    B_Sorting.js

--- B_Sorting.js
+++ B_Sorting.js
@@ -1,19 +1,18 @@
-function bubbleSortDescending(arr) {
-    const n = arr.length;
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] < arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
+rl.question('Enter numbers separated by spaces: ', (input) => {
+  const inputArray = input.split(' ').map(Number);
+  
+  if (Array.isArray(inputArray) && inputArray.every(Number.isFinite)) {
+    const sortedArray = inputArray.sort((a, b) => b - a);
+    console.log('Sorted Array (Descending Order):', sortedArray);
+  } else {
+    console.log('Invalid input. Please enter numbers separated by spaces.');
+  }
 
-const numbers = [5, 2, 9, 1, 5, 6];
-bubbleSortDescending(numbers);
-console.log(numbers); 
-
+  rl.close();
+});
